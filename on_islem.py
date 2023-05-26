4b8fade96008b597fafcfc03ca04b7ab4d02e50f
mubbum <melihutkubala@gmail.com>
2023-05-27 00:23:40 +0300

    on_islem.py eklendi

--- on_islem.py
+++ on_islem.py
@@ -65,67 +65,6 @@ def pre_processing(value):
                               (remove_username
                                (stem_word(word)))))))) for word in value.split()]
 
-df["Text"].apply(pre_processing)
 # Boşlukların kaldırılması
 def remove_space(value):
     return [item for item in value if itrm.strip()]
-
-# word2vec model oluşturma ve kaydetme
-def word2vec_create(value):
-    model = Word2Vec(sentences = value.tolist(),vector_size=100,window=5,min_count=1)
-    model.save("data/word2vec.model")
-
-# word2vec model yükleme ve vektör çıkarma
-def word2vec_analysis(value):
-    model = Word2Vec.load("data/word2vec.model")
-    pass
-
-# word2vec model güncellenir.
-def word2vec_update(value):
-    model = Word2Vec.load("data/word2vec.model")
-    model.build_vocab(value.tolist(),update=True)
-    model.save("data/word2vec.model")
-
-
-if __name__ == '__main__':
-   
-    # veri temizlemesi için örnek veri kümemiz okunur.
-    df_1 = pd.read_csv("data/nlp.csv",index_col=0)
-
-
-    ### tanımlanan df_1 içerisinde Text sütununu ön işlem fonksiyonlarından geçirerek Text_2 olarak df_1 içerisinde yeni bir sütun oluşturun. ###
-    
-
-    ### df_1 içerisinde Text_2 sütununda boş liste kontrolü ###
-
-    
-    ### word2vec model oluşturma ###
-    
-    
-    # df_1 dataframe mizi artık kullanmaycağımızdan ram de yer kaplamaması adına boş bir değer ataması yapıyoruz.
-    df_1 = {}
-
-    #############################################################################################################################################
-
-    # sınıflandırma yapacağımız veri okunur.
-    df_2 = pd.read_csv("data/metin_siniflandirma.csv",index_col=0)
-
-    ### tanımlanan df_2 içerisinde Text sütununu ön işlem fonksiyonlarından geçirerek Text_2 olarak df_2 içerisinde yeni bir sütun oluşturun. ###
-
-    
-    ### df_2 içerisinde Text_2 sütununda boş liste kontrolü ###
-
-
-    ### sınıflandırma yapacağımız df_2 içerisinde bulunan Text_2 sütun verisini word2vec verisinde güncelleyin. ### 
-
-
-    ### Text_2 sütun üzerinden word2vec adında bu modeli kullanarak yeni bir sütun yaratın
-
-
-    ### word2vec sütunumuzu train test olarak bölün ###
-
-
-    ### svm pipeline oluştur, modeği eğit ve test et ###
-
-    
-    ### accuracy ve f1 score çıktısını print ile gösterin. ###
